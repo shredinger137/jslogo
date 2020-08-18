6f3a9751091e063eac6c3e789f15c274ff741861
Casey Lewiston <lewiston@sonoma.edu>
2020-08-18 13:17:00 -0700

    Old button

--- site/src/App.js
+++ site/src/App.js
@@ -136,17 +136,13 @@ class App extends Component {
       <header className="header">
       <h1>Learning by Making</h1>
     </header>
-    <div className="main">      <br />
+    <div className="main">
     <p>Click 'connect' to start, then select the Arduino device. After that you can use the below buttons, or enter commands in the terminal (bottom right). Defining a 'go' word allows you to run
       things by clicking 'go'. Note that this only works in Chrome.
       <br />
     </p>
       <button id="connectButton" type="button" >Connect</button>
-      <button id="readADC" type="button" onClick={() => { this.sendToBoard(0xc0) }}>Read ADC0</button>
-      <button id="dp3on" type="button" onClick={() => { this.sendToBoard(0xe3) }}>dp3on</button>
-      <button id="dp3off" type="button" onClick={() => { this.sendToBoard(0xd3) }}>dp3off</button>
       <button id="gobutton" onClick={() => {megaclass.runLine("go")}}>Go</button>
-      <br />
       <br />
       <br />
     </div>
