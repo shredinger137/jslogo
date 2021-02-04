8ab54fbd02be73b6f501020388509f38d11fd3c0
Casey Lewiston <lewiston@sonoma.edu>
2021-02-04 14:01:16 -0800

    update test

--- site/src/App.js
+++ site/src/App.js
@@ -78,7 +78,7 @@ end`,
 
     //TODO: The 'component did mount' refers to app.js, not turtlelogo.js; meaning the elements aren't correct.
 
-
+    console.log("update");
 
     localDatabase = new Dexie('lbym');
 
--- site/src/index.js
+++ site/src/index.js
@@ -18,24 +18,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function test() {
-  console.log("update");
-}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({
-  onUpdate: registration => {
-    const waitingServiceWorker = registration.waiting
-
-    if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener("statechange", event => {
-        if (event.target.state === "activated") {
-          window.location.reload()
-        }
-      });
-      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
-    }
-  }
+  onUpdate: window.location.reload()
 });
