af0b43e367c039b2ed13e9236b1d3bf7c0aad27d
Casey Lewiston <lewiston@sonoma.edu>
2021-03-10 10:20:29 -0800

    Showing config

---
+++ site/src/config.js
@@ -0,0 +1,11 @@
+export const config = {
+  apiUrl: 'http://localhost:3224',
+  firebase: {
+      apiKey: "AIzaSyBELlFvBiDtHID6Lo7pBKzDQ6tWL4kHAfs",
+      authDomain: "lbym-8be59.firebaseapp.com",
+      projectId: "lbym-8be59",
+      storageBucket: "lbym-8be59.appspot.com",
+      messagingSenderId: "67087581587",
+      appId: "1:67087581587:web:ea96ee610de15eff7e2866"
+    }
+}
