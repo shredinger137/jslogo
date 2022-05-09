7988abc85b68c9e7ee2b034aeca1cd9147a08c4b
Casey Lewiston <lewiston@sonoma.edu>
2022-05-09 12:17:47 -0700

    cleanup

--- site/src/components/OpenDataModal.js
+++ site/src/components/OpenDataModal.js
@@ -1,59 +1,68 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { config } from '../config';
 
 const OpenDataModal = (props) => {
 
-const [availableData, setAvailableData] = useState([]);
+    const [availableData, setAvailableData] = useState([]);
 
-useEffect(() => {
+    useEffect(() => {
 
         axios.get(`${config.apiUrl}/data/${props.pid}`, {
         }).then(response => {
-           if(response && response.data && Array.isArray(response.data)){
-               setAvailableData(response.data)
-           }
+            if (response && response.data && Array.isArray(response.data)) {
+                setAvailableData(response.data)
+            }
         })
-    
-},
-    []
-)
 
-const getData = (index) => {
-    axios.get(`${config.apiUrl}/data/${props.pid}/${index}`, {
-    }).then(response => {
-       if(response && response.data){          
-           console.log(response.data)
-           props.setTableData(response.data);
-           props.toggleModal();
-       }
-    })
-}
+    },
+        []
+    )
 
-const convertDate = (unixtime) => {
-    let date = new Date(unixtime);
-    let converted = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    return converted;
-}
+    const getData = (index) => {
+        axios.get(`${config.apiUrl}/data/${props.pid}/${index}`, {
+        }).then(response => {
+            if (response && response.data) {
+                props.setTableData(response.data);
+                props.toggleModal();
+            }
+        })
+    }
+
+    const convertDate = (unixtime) => {
+
+        //takes number, returns string
+        const twoDigit = (input) => {
+            if (input < 10) {
+                return `0${input}`
+            }
+            return input;
+        }
+
+        let date = new Date(unixtime);
+        let minutes = twoDigit(date.getMinutes());
+        let month = date.getMonth() + 1;
+        let converted = `${twoDigit(date.getMonth())}-${twoDigit(date.getDate())}-${date.getFullYear()} ${twoDigit(date.getHours())}:${twoDigit(date.getMinutes())}:${twoDigit(date.getSeconds())}`;
+        return converted;
+    }
 
 
-return (
-    <div id="dataModal" className="modal" onClick={() => props.toggleModal()}>
-    <div className="modalContent" onClick={(e) => e.stopPropagation()}>
-        <span className="close" onClick={() => props.toggleModal()}>&times;</span>
-        <h3>Load Data</h3>
-        <br />
-        <div>
-            <p>This feature is under development and shouldn't be relied on quite yet.</p>
-            {availableData.map(item => (
-                <div key={item} onClick={() => {getData(item)}}>
-                {convertDate(item)}<br/><br />
+    return (
+        <div id="dataModal" className="modal" onClick={() => props.toggleModal()}>
+            <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+                <span className="close" onClick={() => props.toggleModal()}>&times;</span>
+                <h3>Load Data</h3>
+                <br />
+                <div>
+                    {availableData.map(item => (
+                        <div key={item} onClick={() => { getData(item) }}>
+                            {convertDate(item)}<br /><br />
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
         </div>
-    </div>
-</div>
-)
+    )
 
 }
 
